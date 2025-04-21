c9cd6ef4f8f7bfebf733e8fb5317b04bd5b33984
Kaarroolinaa <kaarroolinaa.kosenko@gmail.com>
2025-04-21 18:25:30 +0300

    Added in-game menu

--- core/src/main/java/com/knightgame/screens/GameScreen.java
+++ core/src/main/java/com/knightgame/screens/GameScreen.java
@@ -6,6 +6,13 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.knightgame.KnightGame;
 
 public class GameScreen implements Screen {
@@ -14,13 +21,19 @@ public class GameScreen implements Screen {
     private Texture knightTexture;
     private Texture backgroundTexture;
 
-    // Позиції та швидкості
+
     private float x, y;
-    private float velocityY = 0;
-    private final float speed = 200f;          // горизонтальна швидкість
-    private final float gravity = 1000f;       // прискорення вільного падіння
-    private final float jumpVelocity = 500f;   // початкова швидкість стрибка
-    private float groundY;                     // рівень "землі" по вісі Y
+    private float velocityY;
+    private final float speed = 200f;
+    private final float gravity = 1000f;
+    private final float jumpVelocity = 500f;
+    private float groundY;
+
+
+    private Stage uiStage;
+    private Skin skin;
+    private Table menuTable;
+    private boolean paused;
 
     public GameScreen(KnightGame game) {
         this.game = game;
@@ -29,64 +42,104 @@ public class GameScreen implements Screen {
     @Override
     public void show() {
         batch = new SpriteBatch();
-        backgroundTexture = new Texture("background.png");
-        knightTexture = new Texture("knight.png");
+        backgroundTexture = new Texture(Gdx.files.internal("background.png"));
+        knightTexture = new Texture(Gdx.files.internal("knight.png"));
 
-        // Земля
         groundY = 0;
-
-        // Початкова позиція лицаря
+        velocityY = 0;
         x = Gdx.graphics.getWidth() / 2f - knightTexture.getWidth() / 2f;
         y = groundY;
+
+
+        uiStage = new Stage(new ScreenViewport());
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+        menuTable = new Table(skin);
+        menuTable.setFillParent(true);
+        menuTable.center();
+
+        TextButton resumeBtn = new TextButton("Resume", skin);
+        TextButton settingsBtn = new TextButton("Settings", skin);
+        TextButton exitBtn = new TextButton("Exit to Menu", skin);
+
+        resumeBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                paused = false;
+                Gdx.input.setInputProcessor(null);
+            }
+        });
+        settingsBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new SettingsScreen(game, GameScreen.this));
+            }
+        });
+        exitBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new MainMenuScreen(game));
+            }
+        });
+
+        menuTable.add(resumeBtn).pad(10).row();
+        menuTable.add(settingsBtn).pad(10).row();
+        menuTable.add(exitBtn).pad(10);
+        uiStage.addActor(menuTable);
+
+        paused = false;
     }
 
     @Override
     public void render(float delta) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            paused = !paused;
+            if (paused) {
+                Gdx.input.setInputProcessor(uiStage);
+            } else {
+                Gdx.input.setInputProcessor(null);
+            }
+        }
+
         ScreenUtils.clear(0, 0, 0, 1);
 
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            x -= speed * delta;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            x += speed * delta;
-        }
+        if (!paused) {
 
-        // Стрибок: тільки коли на землі
-        if ((Gdx.input.isKeyJustPressed(Input.Keys.W) || Gdx.input.isKeyJustPressed(Input.Keys.SPACE))
-            && y <= groundY + 0.1f) {
-            velocityY = jumpVelocity;
-        }
+            if (Gdx.input.isKeyPressed(Input.Keys.A)) x -= speed * delta;
+            if (Gdx.input.isKeyPressed(Input.Keys.D)) x += speed * delta;
+            if ((Gdx.input.isKeyJustPressed(Input.Keys.W) || Gdx.input.isKeyJustPressed(Input.Keys.SPACE))
+                && y <= groundY + 0.1f) {
+                velocityY = jumpVelocity;
+            }
 
-        // Фізика гравітації
-        velocityY -= gravity * delta;    // прискорення вниз
-        y += velocityY * delta;          // зміна висоти
-
-        // Обмежуємо знизу землею
-        if (y < groundY) {
-            y = groundY;
-            velocityY = 0;
+            velocityY -= gravity * delta;
+            y += velocityY * delta;
+            if (y < groundY) {
+                y = groundY;
+                velocityY = 0;
+            }
         }
 
-        // Малювання
+
         batch.begin();
-
         batch.draw(backgroundTexture, 0, 0,
             Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
         batch.draw(knightTexture, x, y);
         batch.end();
+
+
+        if (paused) {
+            uiStage.act(delta);
+            uiStage.draw();
+        }
     }
 
     @Override
     public void resize(int width, int height) {
-
+        uiStage.getViewport().update(width, height, true);
     }
 
-    @Override
-    public void pause() { }
-
-    @Override
-    public void resume() { }
+    @Override public void pause() {}
+    @Override public void resume() {}
 
     @Override
     public void hide() {
@@ -98,5 +151,7 @@ public class GameScreen implements Screen {
         batch.dispose();
         knightTexture.dispose();
         backgroundTexture.dispose();
+        uiStage.dispose();
+        skin.dispose();
     }
 }
--- core/src/main/java/com/knightgame/screens/SettingsScreen.java
+++ core/src/main/java/com/knightgame/screens/SettingsScreen.java
@@ -12,18 +12,21 @@ import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.knightgame.KnightGame;
 
+/**
+ * Екран налаштувань з підтримкою повернення на вказаний екран.
+ */
 public class SettingsScreen implements Screen {
     private final KnightGame game;
+    private final Screen returnScreen;
     private Stage stage;
     private Skin skin;
     private Texture grayTexture;
-
     private Slider volumeSlider;
     private CheckBox muteCheckbox;
     private TextButton skipTutorialButton;
@@ -31,86 +34,57 @@ public class SettingsScreen implements Screen {
     private TextButton saveButton;
     private TextButton backButton;
 
+    /**
+     * Використовується при вході з головного меню: повернення веде в MainMenuScreen.
+     */
     public SettingsScreen(KnightGame game) {
+        this(game, new MainMenuScreen(game));
+    }
+
+    /**
+     * Загальний конструктор, де можна вказати, куди повернутись натисканням "Back".
+     */
+    public SettingsScreen(KnightGame game, Screen returnScreen) {
         this.game = game;
+        this.returnScreen = returnScreen;
     }
 
     @Override
     public void show() {
-        // Створюємо сцену і обробник вводу
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
 
-        // Завантажуємо скіни
         skin = new Skin(Gdx.files.internal("uiskin.json"));
-
-        // Створюємо таблицю для розмітки
         Table table = new Table(skin);
         table.setFillParent(true);
         table.top();
         stage.addActor(table);
 
-        // Додаємо сірий фон
+        // Фон
         grayTexture = new Texture(Gdx.files.internal("gray_background.png"));
-        TextureRegionDrawable bg = new TextureRegionDrawable(new TextureRegion(grayTexture));
-        table.setBackground(bg);
+        table.setBackground(new TextureRegionDrawable(new TextureRegion(grayTexture)));
 
-        // Volume Slider
+        // Елементи налаштувань
         volumeSlider = new Slider(0f, 1f, 0.01f, false, skin);
-        volumeSlider.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, com.badlogic.gdx.scenes.scene2d.Actor actor) {
-                // тут можна оновити налаштування звуку
-                // game.getSettings().setVolume(volumeSlider.getValue());
-            }
-        });
-
-        // Mute Checkbox
         muteCheckbox = new CheckBox("Mute Sound", skin);
-        muteCheckbox.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, com.badlogic.gdx.scenes.scene2d.Actor actor) {
-                // game.getSettings().setMuted(muteCheckbox.isChecked());
-            }
-        });
-
-        // Skip Tutorial Button
         skipTutorialButton = new TextButton("Skip Tutorial", skin);
-        skipTutorialButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                // game.getSettings().setTutorialSkipped(true);
-            }
-        });
-
-        // Restart Level Button
         restartLevelButton = new TextButton("Restart Level", skin);
-        restartLevelButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                // game.restartLevel();
-            }
-        });
 
-        // Save Settings Button
         saveButton = new TextButton("Save Settings", skin);
         saveButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                saveSettings();
+            @Override public void clicked(InputEvent event, float x, float y) {
+                // логіка збереження...
             }
         });
 
-        // Back Button
         backButton = new TextButton("Back", skin);
         backButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                game.setScreen(new MainMenuScreen(game));
+            @Override public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(returnScreen);
             }
         });
 
-        // Розміщуємо всі елементи в таблиці
+        // Розміщення в таблиці
         table.pad(20);
         table.add("Settings").colspan(2).padBottom(20).row();
         table.add("Volume").left();
@@ -124,44 +98,22 @@ public class SettingsScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        // Очищуємо екран перед малюванням
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
+        ScreenUtils.clear(0, 0, 0, 1);
         stage.act(delta);
         stage.draw();
     }
 
-    @Override
-    public void resize(int width, int height) {
+    @Override public void resize(int width, int height) {
         stage.getViewport().update(width, height, true);
     }
-
-    @Override
-    public void pause() { }
-
-    @Override
-    public void resume() { }
-
-    @Override
-    public void hide() {
-        dispose();
-    }
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() { dispose(); }
 
     @Override
     public void dispose() {
         stage.dispose();
         skin.dispose();
         grayTexture.dispose();
-    }
-
-    // Метод для збереження налаштувань гри
-    private void saveSettings() {
-        float volume = volumeSlider.getValue();
-        boolean isMuted = muteCheckbox.isChecked();
-        // Приклад: зберігаємо в налаштуваннях гри або Preferences
-        // game.getSettings().setVolume(volume);
-        // game.getSettings().setMuted(isMuted);
-        System.out.println("Settings saved: Volume = " + volume + ", Mute = " + isMuted);
     }
 }
